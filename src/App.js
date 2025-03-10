e13677e60a75b1ac9f3a9029e442d1d1510021ce
Mickey9315 <taki.mickey9315@gmail.com>
2025-03-10 03:08:49 -0400

    first chage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>2nd 2nd sdfsdfsdf2nd !!!!</h1>
+        <h1>Hello ,CI/CD</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
